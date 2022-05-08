3328acc75b6d9e87e2dd63ab0bd0557f6ef752fd
Christopher Geary <hello@crgeary.com>
2022-05-08 16:32:40 +0100

    Wrap page in container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import { NextPage } from "next";
 import { Game } from "../components/game/game.component";
 
-const IndexPage = () => {
-    return <Game />;
+const IndexPage: NextPage = () => {
+    return (
+        <div className="container mx-auto p-1">
+            <Game />
+        </div>
+    );
 };
 
 export default IndexPage;
