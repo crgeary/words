d5594b7d54c8e3010493685dbf03207a4ab921be
Christopher Geary <hello@crgeary.com>
2022-05-08 15:18:29 +0100

    Add row component

---
+++ src/components/row/row.component.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import { VALID_WORD_LENGTH } from "../../constants";
+
+import { Tile, TileProps } from "../tile/tile.component";
+
+export type RowProps = {
+    letters: Pick<TileProps, "children" | "state">[];
+};
+
+export const Row: FC<RowProps> = ({ letters }) => {
+    return (
+        <div className="grid grid-cols-5 gap-1">
+            {Array.from({ ...letters, length: VALID_WORD_LENGTH }).map((letter, i) => (
+                <Tile key={i} {...letter} />
+            ))}
+        </div>
+    );
+};
---
+++ src/components/row/row.test.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import { Row } from "./row.component";
+
+test("it should match snapshot", () => {
+    const letters = "hello";
+    const { container } = render(
+        <Row letters={letters.split("").map((children) => ({ children }))} />,
+    );
+    expect(container.firstChild).toMatchSnapshot();
+});
