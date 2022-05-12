4b48bf95ca60e411f28628484bb9d2a94794b663
Christopher Geary <hello@crgeary.com>
2022-05-12 22:25:19 +0100

    Add WIP / MVP game component

--- src/components/game/game.component.tsx
+++ src/components/game/game.component.tsx
@@ -1,9 +1,89 @@
-import { FC } from "react";
+import classNames from "classnames";
+import { FC, useEffect, useState } from "react";
+import { VALID_WORD_LENGTH } from "../../constants";
+import { useNumber } from "../../hooks/use-number";
+import { GameState } from "../../types/game-state";
+import { isValidWord } from "../../utils/is-valid-word";
+import { Board } from "../board/board.component";
+import { Keyboard } from "../keyboard/keyboard.component";
 
 export type GameProps = {
     solution: string;
 };
 
 export const Game: FC<GameProps> = ({ solution }) => {
-    return <div>...</div>;
+    const rowCount = useNumber({ defaultValue: 5, min: 5, max: 10 });
+    const [guesses, setGuesses] = useState<string[]>([]);
+    const [currentGuess, setCurrentGuess] = useState("");
+    const [status, setStatus] = useState<GameState>(GameState.Idle);
+
+    const addGuess = () => {
+        if (
+            guesses.length >= rowCount.value ||
+            guesses.includes(currentGuess) ||
+            !isValidWord(currentGuess)
+        ) {
+            return;
+        }
+        setGuesses((v) => [...v, currentGuess]);
+        setCurrentGuess("");
+    };
+
+    useEffect(() => {
+        if (guesses.length === 0) {
+            setStatus(GameState.Idle);
+        } else if (guesses.at(-1) === solution) {
+            setStatus(GameState.Success);
+        } else if (guesses.length >= rowCount.value && guesses.at(-1) !== solution) {
+            setStatus(GameState.Fail);
+        } else {
+            setStatus(GameState.Playing);
+        }
+    }, [guesses, rowCount.value, solution]);
+
+    return (
+        <div>
+            <div
+                className={classNames("p-3 uppercase text-center", {
+                    "bg-slate-100": [GameState.Idle, GameState.Playing].includes(status),
+                    "bg-red-600": GameState.Fail === status,
+                    "bg-greens-600": GameState.Success === status,
+                })}
+            >
+                {status}
+                {status === GameState.Success && (
+                    <button
+                        className="border p-1"
+                        onClick={() => {
+                            // ..
+                            setGuesses([]);
+                        }}
+                    >
+                        Play Again
+                    </button>
+                )}
+            </div>
+            <div className="my-2">
+                <Board
+                    rows={rowCount.value}
+                    solution={solution}
+                    guesses={guesses}
+                    currentGuess={currentGuess}
+                />
+            </div>
+            <Keyboard
+                solution={solution}
+                guesses={guesses}
+                onChar={(char) => {
+                    if (currentGuess.length < VALID_WORD_LENGTH) {
+                        setCurrentGuess((guess) => `${guess}${char}`);
+                    }
+                }}
+                onEnter={() => addGuess()}
+                onDelete={() => {
+                    setCurrentGuess((guess) => guess.substring(0, guess.length - 1));
+                }}
+            />
+        </div>
+    );
 };
---
+++ src/types/game-state.ts
@@ -0,0 +1,6 @@
+export enum GameState {
+    Idle = "idle",
+    Playing = "playing",
+    Fail = "fail",
+    Success = "success",
+}
