b4d206efc2f4448a20f460a6597e41a338ad5b82
Christopher Geary <hello@crgeary.com>
2022-05-14 00:02:17 +0100

    Add Inter font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,8 +7,12 @@ class MyDocument extends Document {
                 <Head>
                     <link rel="manifest" href="/manifest.webmanifest" />
                     <link rel="icon" type="image/svg+xml" href="/images/icon.svg" />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
+                    />
                 </Head>
-                <body className="bg-slate-50">
+                <body className="bg-slate-50 font-sans">
                     <Main />
                     <NextScript />
                 </body>
