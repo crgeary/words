df063eadb808cb53295c2363a575aa9f7c0a2ab6
Christopher Geary <hello@crgeary.com>
2022-05-14 00:46:54 +0100

    Tweak tile layout/design

--- src/components/tile/tile.component.tsx
+++ src/components/tile/tile.component.tsx
@@ -9,14 +9,17 @@ export type TileProps = PropsWithChildren<{
 export const Tile: FC<TileProps> = ({ children, state = CharState.Idle }) => {
     return (
         <span
-            className={classNames("inline-block rounded w-full h-full p-2 text-center", {
-                "bg-green-600 text-white": CharState.Correct === state,
-                "bg-yellow-600 text-white": CharState.Present === state,
-                "bg-slate-600 text-white": CharState.Absent === state,
-                "bg-slate-300 text-gray-700": CharState.Idle === state,
-            })}
+            className={classNames(
+                "inline-flex items-center justify-center rounded w-full h-full p-2",
+                {
+                    "bg-green-600 text-white": CharState.Correct === state,
+                    "bg-yellow-600 text-white": CharState.Present === state,
+                    "bg-slate-600 text-white": CharState.Absent === state,
+                    "bg-slate-300 text-gray-700": CharState.Idle === state,
+                },
+            )}
         >
-            {children && <span className="uppercase">{children}</span>}
+            {children && <span className="uppercase font-semibold text-xl">{children}</span>}
         </span>
     );
 };
